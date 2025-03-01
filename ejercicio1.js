52d0c2917d45110c42f88e4c2f42fa6a436f3096
MDuran21 <ciber.mduran@gmail.com>
2025-03-01 18:16:29 +0100

    First commit

---
+++ ejercicio1.js
@@ -0,0 +1,23 @@
+const characters = [
+  { name: 'Luke Skywalker', species: 'Human' },
+  { name: 'Darth Vader', species: 'Human' },
+  { name: 'Chewbacca', species: 'Wookiee' },
+  { name: 'Leia Organa', species: 'Human' },
+  { name: 'R2-D2', species: 'Droid' },
+  { name: 'C-3PO', species: 'Droid' },
+  { name: 'Obi-Wan Kenobi', species: 'Human' },
+  { name: 'Yoda', species: 'Unknown' },
+  { name: 'Han Solo', species: 'Human' }
+]
+const humanCharacters = []
+
+getHumanCharacters(characters)
+console.log(humanCharacters)
+
+function getHumanCharacters(characters) {
+  for (let i = 0; i < characters.length; i++) {
+    if (characters[i].species === 'Human') {
+      humanCharacters.push(characters[i])
+    }
+  }
+}
---
+++ ejercicio2.js
@@ -0,0 +1,29 @@
+const movies = [
+  { title: 'The Matrix', releaseYear: 1999 },
+  { title: 'Star Wars: Episode IV – A New Hope', releaseYear: 1977 },
+  { title: 'Inception', releaseYear: 2010 },
+  { title: 'Jurassic Park', releaseYear: 1993 },
+  { title: 'The Shawshank Redemption', releaseYear: 1994 },
+  { title: 'Pulp Fiction', releaseYear: 1994 },
+  { title: 'Avatar', releaseYear: 2009 },
+  { title: 'The Dark Knight', releaseYear: 2008 },
+  { title: 'Fight Club', releaseYear: 1999 },
+  { title: 'Forrest Gump', releaseYear: 1994 }
+]
+
+let moviesBefore2000 = 0
+let moviesAfter2000 = 0
+
+getMoviesBeforAndAfter(movies)
+console.log(`Before 2000: ${moviesBefore2000}`)
+console.log(`After 2000: ${moviesAfter2000}`)
+
+function getMoviesBeforAndAfter(movies) {
+  for (const movie of movies) {
+    if (movie.releaseYear < 2000) {
+      moviesBefore2000++
+    } else {
+      moviesAfter2000++
+    }
+  }
+}
